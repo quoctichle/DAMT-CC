e5fc66efeb1f180a6dba6d5900fcfe21a68d42aa
quoctichle <quoctichle@gmail.com>
2023-04-04 12:16:47 +0700

    sửa lỗi Parameter name: entitySet

---
+++ WebSach/WebSach/Areas/WebAdmin/Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSach.Areas.WebAdmin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: WebAdmin/HomeAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
